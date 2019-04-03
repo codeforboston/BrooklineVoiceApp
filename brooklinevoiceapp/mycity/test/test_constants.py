86ea33ed7c71ca6138c4f134fb099f8f790751a3
Jeff A <jeffrey.amari001@umb.edu>
2019-04-02 20:56:40 -0400

    Add unit tests for arcgis utils (#26)
    * Add functionality for Closest Police Stations. Currently returns a dummy value because we are not yet interacting with ArcGIS API
    
    * complete happy path for police station intent
    
    * add missing helper
    
    * remove the darn tabs
    
    * address review comments
    
    * add nearest police station intent test
    
    * edit gitignore
    
    * add unit tests for arcgis utils
    
    * address review comments
    
    * address review comments

--- brooklinevoiceapp/mycity/test/test_constants.py
+++ brooklinevoiceapp/mycity/test/test_constants.py
@@ -69,6 +69,66 @@ NO_RESULTS_GET_POLICE_STATION_API_MOCK = {
     "features": []
 }
 
+GEOCODE_MOCK = {
+    "spatialReference": {
+        "wkid": 4326,
+        "latestWkid": 4326
+    },
+    "x": -71.120614336337198,
+    "y": 42.334020535512529,
+    "z": 0
+}
+
+LOCATION_MOCK = {
+    "x": -71.120614336337198,
+    "y": 42.334020535512529,
+    "z": 0
+}
+
+SPACIAL_REFERENCE_MOCK = {
+    "wkid": 4326,
+    "latestWkid": 4326
+}
+
+ADDRESS_CANDIDATE_MOCK = {
+    "address": "333 WASHINGTON ST",
+    "location": {
+        "x": -71.120614336337198,
+        "y": 42.334020535512529,
+        "z": 0
+    },
+    "score": 100,
+    "attributes": {
+        "Loc_name": "Building",
+        "Score": 100,
+        "Match_addr": "333 WASHINGTON ST"
+    }
+}
+
+GET_ADDRESS_CANDIDATES_API_MOCK = {
+    "spatialReference": {
+        "wkid": 4326,
+        "latestWkid": 4326
+    },
+    "candidates": [
+        {
+            "address": "333 WASHINGTON ST",
+            "location": {
+                "x": -71.120614336337198,
+                "y": 42.334020535512529,
+                "z": 0
+            },
+            "score": 100,
+            "attributes": {
+                "Loc_name": "Building",
+                "Score": 100,
+                "Match_addr": "333 WASHINGTON ST"
+            }
+        }
+    ]
+}
+
+GEOCODE_ADDRESS_MOCK = [42.35351814, -71.13117064]
 
 GET_TRASH_PICKUP_API_MOCK = {
     "displayFieldName": "DISTRICTID",
@@ -145,3 +205,4 @@ NO_RESPONSE_TRASH_PICKUP_API_MOCK = {
     },
     "features": []
 }
+
--- brooklinevoiceapp/mycity/utils/brookline_arcgis_api_utils.py
+++ brooklinevoiceapp/mycity/utils/brookline_arcgis_api_utils.py
@@ -3,6 +3,8 @@ from enum import Enum
 import json
 import logging
 import requests
+import logging
+import typing
 
 
 logger = logging.getLogger(__name__)
@@ -31,6 +33,7 @@ class MapFeatureID(Enum):
     POLICE_STATION = 10
     TRASH_DAY = 12
 
+
 CANDIDATES_PATH = "candidates"
 LOCATION_PATH = "location"
 X_PATH = "x"
@@ -39,64 +42,40 @@ FULL_ADDR_PATH = "FULLADDR"
 SPATIAL_REFERENCE_PATH = "spatialReference"
 
 
-def get_nearest_police_station_json(address):
+
+def get_first_address_candidate(address: str,
+    _requests: typing.ClassVar = requests) -> object:
     """
-    Queries the Brookline arcgis server for the nearest police station
+    Retrieves the first address candidate for a given address
 
     :param address: Address string to query
-    :return: Json data object response
-    """
-    logger.debug('Finding closest police station for address: ' + str(address))
-    return get_nearest_feature_json(address, MapFeatureID.POLICE_STATION)
-
-def get_trash_day_json(address):
-    """
-    Queries the Brookline arcgis server for trash day
-
-    :param address: Address string to query
-    :return: Json object response from the server
-    """
-    logger.debug('Finding trash day for address: ' + str(address))
-    return get_nearest_feature_json(address, MapFeatureID.TRASH_DAY)
-
-def get_nearest_feature_json(address, map_feature_id):
-    """
-    Gets the information of the provided map feature from Brookline argis server
-
-    :param address: Adress string to use in query
-    :param map_feature_id: MapFeatureID to query the server for
-    :return: Json data object response
+    :param _requests: Requests library object
+    :return: Json response object address and spatial reference
     """
 
-
-    if not isinstance(map_feature_id, MapFeatureID):
-        raise Exception('get_nearest_feature_json() called with invalid feature ID')
-
-    url = MAPSERVER_URL.format(map_feature_id.value)
-    headers = {CONTENT_TYPE_HEADER: "application/x-www-form-urlencoded"}
-    payload = {
+    url_params = {
         F_PARAM: "json",
-        RETURN_GEOMETRY_PARAM: "true",
-        SPATIAL_REL_PARAM: "esriSpatialRelIntersects",
-        GEOMETRY_TYPE_PARAM: "esriGeometryPoint",
-        INSR_PARAM: "102100",
+        SINGLE_LINE_PARAM: address,
         OUT_FIELDS_PARAM: "*",
-        OUTSR_PARAM: "102100",
-        GEOMETRY_PARAM: json.dumps(geocode_address(address))
+        OUTSR_PARAM: "102100"
     }
-    with requests.Session() as session:
-        response = session.post(url, headers=headers, data=payload)
+    with _requests.Session() as session:
+        response = session.get(GEOCODE_URL, params=url_params)
 
-    logger.debug('Got response from Brookline arcgis: ' + str(response))
-    return response.json()
+    logger.debug('Got address candidate response: ' + str(response))
+    candidate_address = response.json()[CANDIDATES_PATH][0]
+    spatial_reference = response.json()[SPATIAL_REFERENCE_PATH]
+    return candidate_address, spatial_reference
 
 
 
-def geocode_address(address):
+def geocode_address(address: str,
+    _get_first_address_candidate: callable = get_first_address_candidate) -> dict:
     """
     Retrieves address candidates with coordinates for a given address
 
     :param address: Address string to query
+    :param _get_first_address_candidate: injectable function for test
     :return: Dictionary of coordinates with associated spatial reference
     """
     try:
@@ -110,24 +89,63 @@ def geocode_address(address):
 
 
 
-def _get_first_address_candidate(address):
+def get_nearest_feature_json(address: str,
+    map_feature_id: MapFeatureID,
+    _requests: typing.ClassVar = requests,
+    _geocode_address: callable = geocode_address) -> object:
     """
-    Retrieves the first address candidate for a given address
+    Gets the information of the provided map feature from Brookline argis server
+
+    :param address: Adress string to use in query
+    :param map_feature_id: MapFeatureID to query the server for
+    :param _requests: Requests library object
+    :param _geocode_address: injectable function for test
+    :return: Json data object response
+    """
+    if not isinstance(map_feature_id, MapFeatureID):
+        raise Exception('get_nearest_feature_json() called with invalid feature ID')
+
+    url = MAPSERVER_URL.format(map_feature_id.value)
+    headers = {CONTENT_TYPE_HEADER: "application/x-www-form-urlencoded"}
+    payload = {
+        F_PARAM: "json",
+        RETURN_GEOMETRY_PARAM: "true",
+        SPATIAL_REL_PARAM: "esriSpatialRelIntersects",
+        GEOMETRY_TYPE_PARAM: "esriGeometryPoint",
+        INSR_PARAM: "102100",
+        OUT_FIELDS_PARAM: "*",
+        OUTSR_PARAM: "102100",
+        GEOMETRY_PARAM: json.dumps(_geocode_address(address))
+    }
+    with _requests.Session() as session:
+        response = session.post(url, headers=headers, data=payload)
+
+    logger.debug('Got response from Brookline arcgis: ' + str(response))
+    return response.json()
+
+
+
+def get_nearest_police_station_json(address: str,
+    _get_nearest_feature_json: callable = get_nearest_feature_json) -> object:
+    """
+    Queries the Brookline arcgis server for the nearest police station
 
     :param address: Address string to query
-    :return: Json response object address and spatial reference
+    :return: Json data object response
     """
+    logger.debug('Finding closest police station for address: ' + str(address))
+    return _get_nearest_feature_json(address, MapFeatureID.POLICE_STATION)
 
-    url_params = {
-        F_PARAM: "json",
-        SINGLE_LINE_PARAM: address,
-        OUT_FIELDS_PARAM: "*",
-        OUTSR_PARAM: "102100"
-    }
-    with requests.Session() as session:
-        response = session.get(GEOCODE_URL, params=url_params)
 
-    logger.debug('Got address candidate response: ' + str(response))
-    candidate_address = response.json()[CANDIDATES_PATH][0]
-    spatial_reference = response.json()[SPATIAL_REFERENCE_PATH]
-    return candidate_address, spatial_reference
+
+def get_trash_day_json(address: str,
+    _get_nearest_feature_json: callable = get_nearest_feature_json) -> object:
+    """
+    Queries the Brookline arcgis server for trash day
+
+    :param address: Address string to query
+    :return: Json object response from the server
+    """
+    logger.debug('Finding trash day for address: ' + str(address))
+    return _get_nearest_feature_json(address, MapFeatureID.TRASH_DAY)
+
