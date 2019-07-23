f2c3b27783f9ee8c47df55fc0e9229848cb73b1b
curranjm <curran.jm@gmail.com>
2019-07-23 19:34:24 -0400

    Add closest location function

---
+++ brooklinevoiceapp/mycity/test/unit_tests/test_address_utils.py
@@ -0,0 +1,14 @@
+from mycity.test import test_constants
+from mycity.test.unit_tests import base
+from mycity.utils import address_utils as utils
+
+
+class AddressUtilsTestCase(base.BaseTestCase):
+
+    def test_get_closest_address(self):
+        expected = test_constants.GET_POLLING_LOCATIONS_API_MOCK['features'][1]
+        actual = utils.get_closest_result(
+            test_constants.GET_POLICE_STATION_API_MOCK['features'][0]['geometry'],
+            test_constants.GET_POLLING_LOCATIONS_API_MOCK['features']
+        )
+        self.assertEqual(expected, actual)
