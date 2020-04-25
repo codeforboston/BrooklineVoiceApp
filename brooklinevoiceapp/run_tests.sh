a5fdf8ff9ad1486e2408bf318b51e4596d1ee70a
Endy Iskandar Imam <imam.endy@gmail.com>
2020-04-24 22:23:58 -0400

    Coveage over .travis.yml, run_tests.sh, and dev_requirements.txt

--- brooklinevoiceapp/run_tests.sh
+++ brooklinevoiceapp/run_tests.sh
@@ -1,2 +1,17 @@
 #!/usr/bin/env sh
-python -m unittest discover -v -s mycity/test
+if command -v coverage
+then
+    python -m coverage run --source='.' -m unittest discover -v -s mycity/test
+
+    if [ $? -eq 0 ]
+    then
+        if command -v codecov
+        then
+            mv .coverage ..
+        else
+            python -m coverage report -m
+        fi
+    fi
+else
+    python -m unittest discover -v -s mycity/test
+fi
