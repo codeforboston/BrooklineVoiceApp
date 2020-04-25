37a6cc9a8322d508acf49ff93c2d9f508f222d5c
Endy Iskandar Imam <imam.endy@gmail.com>
2020-04-25 00:26:21 -0400

    run_tests.sh return with exit code 1 if fail for TravisCI

--- brooklinevoiceapp/run_tests.sh
+++ brooklinevoiceapp/run_tests.sh
@@ -1,17 +1,27 @@
 #!/usr/bin/env sh
+
+# Test if 'coverage' command exists
 if command -v coverage
 then
+    # test w/ coverage
     python -m coverage run --source='.' -m unittest discover -v -s mycity/test
 
+    # if exit code is 0, test is success and continue on, else exit w/ code
     if [ $? -eq 0 ]
     then
+        # test if 'codecov' in TravisCI exists
         if command -v codecov
         then
+            # move .coverage report file to root
             mv .coverage ..
         else
+            # just show coverage report for local
             python -m coverage report -m
         fi
+    else
+        exit $?
     fi
 else
+    # run the old command
     python -m unittest discover -v -s mycity/test
 fi
